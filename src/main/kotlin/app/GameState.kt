ef950032b7438f6193bba5a9bb44b8dcc06e04e8
Maxim Gavrilov <maksim.gavrilov.0@mail.ru>
2026-04-17 11:04:29 +0300

    feat: GameState class

---
+++ src/main/kotlin/app/GameState.kt
@@ -0,0 +1,45 @@
+package app
+
+/*
+ * GameState class is responsible for choosing the next player's turn
+ * and for player's operations like setting a Meeple and getting score.
+ */
+class GameState (private val players: Array<Player>){
+    private var curPlayerIndex: Int = 0
+
+    init {
+        require(players.size in 1..5)
+    }
+
+    private fun findPlayer(color: Color): Player {
+        return players.find { it.color == color } ?: throw NoSuchElementException("There is no player with such color.")
+    }
+
+    fun nextTurn() {
+        curPlayerIndex = (curPlayerIndex + 1) % players.size
+    }
+
+    fun getPlayerScore(): Int {
+        return players[curPlayerIndex].score
+    }
+
+    fun getPlayerName(): String {
+        return players[curPlayerIndex].name
+    }
+
+    fun getPlayerColor(): Color {
+        return players[curPlayerIndex].color
+    }
+
+    fun addPlayerScore(color: Color, score: Int) {
+        findPlayer(color).addScore(score)
+    }
+
+    fun setPlayerMeeple() {
+        TODO("Need to implement a board.")
+    }
+
+    fun unsetPlayerMeeple(color: Color) {
+        TODO("Need to implement a board.")
+    }
+}

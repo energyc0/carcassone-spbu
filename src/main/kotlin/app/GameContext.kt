7edb47e2faa0605d6771b55045a704ee7a58dcbf
Maxim Gavrilov <maksim.gavrilov.0@mail.ru>
2026-04-17 18:21:42 +0300

    feat: implemented initialization of GameContext

--- src/main/kotlin/app/GameContext.kt
+++ src/main/kotlin/app/GameContext.kt
@@ -1,6 +1,17 @@
 package app
 
-class GameContext (private val gameState: IGameState, private val deck: Deck, private val board: GameBoard){
+class GameContext (playerInitializer: IPlayersInitializer, tilesLoader: IGameTilesLoader, gameRules: IGameRules){
+    private val gameState: IGameState
+    private val deck: Deck
+    private val board: GameBoard
+
+    init {
+        val players = playerInitializer.collectPlayers()
+        gameState = GameState(players)
+        deck = Deck(tilesLoader.loadTiles())
+        board = GameBoard(gameRules)
+    }
+
     fun nextTurn() {
         TODO("Not implemented.")
     }

bea80862608aa11af131388393a254a43d6f6768
Maxim Gavrilov <maksim.gavrilov.0@mail.ru>
2026-04-18 12:43:24 +0300

    feat: implemented canConnect() in GameRules

--- src/main/kotlin/app/GameRules.kt
+++ src/main/kotlin/app/GameRules.kt
@@ -6,6 +6,32 @@ class GameRules : IGameRules {
         to: Tile,
         from: Direction,
     ): Boolean {
-        TODO("Need to implement.")
+        // From left to right
+        val topEdge = arrayOf<TileAreaDir>(TileAreaDir.CORNER_TOP_LEFT, TileAreaDir.TOP_LEFT, TileAreaDir.TOP, TileAreaDir.TOP_RIGHT, TileAreaDir.CORNER_TOP_RIGHT)
+        // From top to bottom
+        val leftEdge = arrayOf<TileAreaDir>(TileAreaDir.CORNER_TOP_LEFT, TileAreaDir.LEFT_TOP, TileAreaDir.LEFT, TileAreaDir.LEFT_BOTTOM, TileAreaDir.CORNER_BOTTOM_LEFT)
+        // From top to bottom
+        val rightEdge = arrayOf<TileAreaDir>(TileAreaDir.CORNER_TOP_RIGHT, TileAreaDir.RIGHT_TOP, TileAreaDir.RIGHT, TileAreaDir.RIGHT_BOTTOM, TileAreaDir.CORNER_BOTTOM_RIGHT)
+        // From left to right
+        val bottomEdge = arrayOf<TileAreaDir>(TileAreaDir.CORNER_BOTTOM_LEFT, TileAreaDir.BOTTOM_LEFT, TileAreaDir.BOTTOM, TileAreaDir.BOTTOM_RIGHT, TileAreaDir.CORNER_BOTTOM_RIGHT)
+
+        val tileEdge = when(from) {
+            Direction.RIGHT -> leftEdge
+            Direction.LEFT -> rightEdge
+            Direction.UP -> bottomEdge
+            Direction.DOWN -> topEdge
+        }
+        val toTileEdge = when(from) {
+            Direction.RIGHT -> rightEdge
+            Direction.LEFT -> leftEdge
+            Direction.UP -> topEdge
+            Direction.DOWN -> bottomEdge
+        }
+
+        for ((i, dir) in tileEdge.withIndex()) {
+            if (tile.getTileArea(dir).type != to.getTileArea(toTileEdge[i]).type)
+                return false
+        }
+        return true
     }
 }

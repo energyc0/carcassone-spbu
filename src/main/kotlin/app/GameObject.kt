57f8b6c4b7a4371e9dd6ac0073b921785553d0d4
Maxim Gavrilov <maksim.gavrilov.0@mail.ru>
2026-04-24 16:18:45 +0300

    feat: expanded GameObject, added factory

--- src/main/kotlin/app/GameObject.kt
+++ src/main/kotlin/app/GameObject.kt
@@ -11,3 +11,38 @@ abstract class GameObject (val type: GameObjectType) {
         meepleCount[color] = meepleCount.getOrDefault(color, 0) + 1
     }
 }
+
+class GameObjectMonastery : GameObject (GameObjectType.MONASTERY){
+    override fun getScore() {
+        TODO("Not yet implemented")
+    }
+}
+
+class GameObjectField : GameObject (GameObjectType.FIELD){
+    override fun getScore() {
+        TODO("Not yet implemented")
+    }
+}
+
+class GameObjectRoad : GameObject (GameObjectType.ROAD){
+    override fun getScore() {
+        TODO("Not yet implemented")
+    }
+}
+
+class GameObjectCity : GameObject (GameObjectType.CITY){
+    override fun getScore() {
+        TODO("Not yet implemented")
+    }
+}
+
+class GameObjectFactory {
+    fun createObject(type: GameObjectType) : GameObject{
+        return when (type) {
+            GameObjectType.CITY -> GameObjectCity()
+            GameObjectType.FIELD -> GameObjectField()
+            GameObjectType.ROAD -> GameObjectRoad()
+            GameObjectType.MONASTERY -> GameObjectMonastery()
+        }
+    }
+}
